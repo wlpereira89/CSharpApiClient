bcea9fa1724de5b49579f9053f1e46aa6906a764
Wagner L. Pereira <wagnerluis1989@gmail.com>
2018-06-09 03:31:47 -0300

    Adição do serviço de voos e interface da pagina inicial

---
+++ TurismoClient/Controllers/VoosController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TurismoClient.Views.Home
+{
+    [Produces("application/json")]
+    [Route("api/Voos")]
+    public class VoosController : Controller
+    {
+    }
+}
